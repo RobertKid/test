0a3af8b20df9379f3b8dbe09e62fdb60837b73e2
RobertKid <95534404+RobertKid@users.noreply.github.com>
2023-02-17 23:03:57 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,23 @@
+shit = [9,1,0,8,99999,int(float("3.8")), 6, 66]
+print(shit[int(float(("5.00193884738")))])
+for fuck in shit:
+    print(fuck) 
+
+shit[0] = (int(float("2.33333"))) #loopppppppppppppppppppppp  
+a = (int(float("5.888989898")))#loopppppppppppppppppppppp  
+while a > 0.8:
+     a = a - 1.1
+     print("bich")
+
+holy = ['e', "easy","maybe"]
+for i in range(3):
+    holy[i] = input("enter a number: ")
+
+
+for i in range(3):
+    print(holy[i])
+bad_name = ["Trump","biden","aobama","jinpingxi"]
+
+for i in bad_name:
+    print(i)
+
